a694d4bec3bea2f4774bf7d2e1ec06ad9351471b
kalmk <111930167+kalmk@users.noreply.github.com>
2024-10-20 10:17:03 -0700

    work

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import foo from "../img/logo.png";
+
+const Container = styled.div`
+  flex: 1;
+  background-color: #202020;
+  height: 100vh;
+  color: white;
+`;
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`;
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`
+
+const Menu = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src= {foo}/>
+          Foo
+        </Logo>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Menu;
